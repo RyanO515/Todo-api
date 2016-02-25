10592f8d1f6f702b4c3431e82e2bd221c2455d72
Ryan <ROliver@zghealth.com>
2016-02-25 16:17:56 -0500

    added delete route to server.js

--- server.js
+++ server.js
@@ -51,6 +51,21 @@ app.post('/todos', function (req, res) {
 	res.json(body);
 });
 
+// DELETE /todos/:id
+app.delete("/todos/:id", function (req, res) {
+    var todoId = Number(req.params.id);
+    var matchedTodo = _.findWhere(todos, {id: todoId});
+
+    if (!matchedTodo) {
+        res.status(404).json({"error": "no todo found"});
+    } else {
+        todos = _.without(todos, matchedTodo);
+        res.json(matchedTodo);
+    }
+
+    
+});
+
 
 app.listen(PORT, function () {
 	console.log("Express listening on port " + PORT + "!");
